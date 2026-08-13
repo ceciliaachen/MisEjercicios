20ff9a98f83e7328f496f595bea88217c76d6e93
ceciliaachen <chencecilia74@gmail.com>
2026-08-13 14:45:44 -0300

    cohete

--- tp2/ejercicio.js
+++ tp2/ejercicio.js
@@ -9,9 +9,34 @@
 // 
 // Se deberá aplicar primero la escala, luego la rotación y finalmente la traslación. 
 // Las rotaciones vienen expresadas en grados. 
-function BuildTransform( positionX, positionY, rotation, scale )
+function BuildTransform(positionX, positionY, rotation, scale )
 {
-	return Array(1,0,0,0,1,0,0,0,1);
+	// Matriz de escalado
+	var scale_mat = new Array(
+		scale, 0, 0,
+		0, scale, 0,
+		0, 0, 1
+	);
+	
+	// Matriz de rotación
+	var tita = rotation * (Math.PI / 180.0); 	// Calculamos tita
+	var rotation_mat = new Array( 
+		Math.cos(tita), Math.sin(tita), 0,
+		-(Math.sin(tita)), Math.cos(tita), 0,
+		0, 0, 1	
+	);
+
+	// Matriz de traslación
+	var translation_mat = new Array(
+		1, 0, 0,
+		0, 1, 0,
+		positionX, positionY, 1
+	);
+
+	var trans1 = ComposeTransforms(scale_mat, rotation_mat);
+	var trans2 = ComposeTransforms(trans1, translation_mat);
+
+	return trans2;
 }
 
 // Esta función retorna una matriz que resula de la composición de trasn1 y trans2. Ambas 
@@ -20,7 +45,18 @@ function BuildTransform( positionX, positionY, rotation, scale )
 // primero trans1 y luego trans2. 
 function ComposeTransforms( trans1, trans2 )
 {
-	return Array(1,0,0,0,1,0,0,0,1);
+	var res = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0);
+	res[0] = trans2[0]*trans1[0] + trans2[3]*trans1[1] + trans2[6]*trans1[2];
+	res[1] = trans2[1]*trans1[0] + trans2[4]*trans1[1] + trans2[7]*trans1[2];
+	res[2] = trans2[2]*trans1[0] + trans2[5]*trans1[1] + trans2[8]*trans1[2];
+	res[3] = trans2[0]*trans1[3] + trans2[3]*trans1[4] + trans2[6]*trans1[5];
+	res[4] = trans2[1]*trans1[3] + trans2[4]*trans1[4] + trans2[7]*trans1[5];
+	res[5] = trans2[2]*trans1[3] + trans2[5]*trans1[4] + trans2[8]*trans1[5];
+	res[6] = trans2[0]*trans1[6] + trans2[3]*trans1[7] + trans2[6]*trans1[8];
+	res[7] = trans2[1]*trans1[6] + trans2[4]*trans1[7] + trans2[7]*trans1[8];
+	res[8] = trans2[2]*trans1[6] + trans2[5]*trans1[7] + trans2[8]*trans1[8];
+	return res;
 }
+
 
 
