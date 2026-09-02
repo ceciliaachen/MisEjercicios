c8deef705ee34e84e7a40dc234d81ddaff192667
ceciliaachen <chencecilia74@gmail.com>
2026-09-02 18:40:35 -0300

    triangulo

--- tp5/triangle_webgl/ejemplo_webgl.js
+++ tp5/triangle_webgl/ejemplo_webgl.js
@@ -1,7 +1,7 @@
 // Código fuente de los shaders
 const vertexShaderSource = `
-	atribute vec3 pos;
-	atribute vec4 clr;
+	attribute vec3 pos;
+	attribute vec4 clr;
 
 	uniform mat4 trans;
 	
@@ -9,7 +9,7 @@ const vertexShaderSource = `
 
 	void main()
 	{
-		gl_Positions = trans * vec4(pos, 1.0);
+		gl_Position = trans * vec4(pos, 1);
 		vcolor = clr;
 	}
 `;
@@ -17,16 +17,11 @@ const vertexShaderSource = `
 const fragmentShaderSource = `
 	precision mediump float;
 
-	varyiing vec4 vcolor;
+	varying vec4 vcolor;
 
 	void main()
 	{
 		gl_FragColor = vcolor;
-	}
-`;
-	void main()
-	{
-		// TODO <-----------
 	}
 `;
 
@@ -91,8 +86,6 @@ window.onload = function()
 		gl.STATIC_DRAW
 	); 
 
-
-	
     /******* 3.0 COMPILAMOS LOS SHADERS *******/
  	// Compilación del vertex shader y del fragment shader.
 	// En caso de que quisieramos cambiar el código de los shaders en algún
@@ -102,24 +95,53 @@ window.onload = function()
 
 	// Vertex shader
 	const vs_source = vertexShaderSource;
-	// TODO <-----------
+	const vs = gl.createShader(gl.VERTEX_SHADER);
+	gl.shaderSource(vs, vs_source);
+	gl.compileShader(vs);
+
+	if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS)) {
+		alert(gl.getShaderInfoLog(vs));
+		gl.deleteShader(vs);
+	}
 
 	// Fragment shader
 	const fs_source = fragmentShaderSource;
-	// TODO <-----------
+	const fs = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(fs, fs_source);
+	gl.compileShader(fs);
 
-	
+	if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) {
+		alert(gl.getShaderInfoLog(fs));
+		gl.deleteShader(fs);
+	}
+
 	// Creo mi "programa" con ambos shaders compilados
-	// TODO <-----------
-	
+	prog = gl.createProgram();
+	gl.attachShader(prog, vs);
+	gl.attachShader(prog, fs);
+	gl.linkProgram(prog);
+
+	// Verifico que el programa se haya linkeado correctamente
+	if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
+		alert(gl.getProgramInfoLog(prog));
+	}
 	
     /******* 4.0 SETEAMOS LAS VARIABLES UNIFORMES  *******/
   	// Actualizamos las variables uniformes para los shaders. 
 	// Antes de reenderizar la escena, es necesario setear las variables
 	// uniformes. Estas variables las podemos actualizar todas las veces
 	// que lo necesitemos. 
-	// TODO <-----------
+	var m = gl.getUniformLocation(prog, 'trans');
 
+	var matrix = [
+		1,0,0,0,
+		0,1,0,0,
+		0,0,1,0,
+		0,0,0,1
+	];
+
+	gl.useProgram(prog);
+	gl.uniformMatrix4fv(m, false, matrix);
 	
     /******* 5.0 LINKEAMOS LOS BUFFERS DE LA ESCENA  *******/
 	// Linkeamos los buffers que son necesarios para el rendering.
@@ -128,18 +150,23 @@ window.onload = function()
 	// diferentes, cada uno con su propio set de atributos. 
 
 	// Link atributo posición
-	// TODO <-----------
+	var p = gl.getAttribLocation(prog, 'pos');
+	gl.bindBuffer(gl.ARRAY_BUFFER, position_buffer);
+	gl.vertexAttribPointer(p, 3, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(p);
 
 
 	// Link atributo color
-	// TODO <-----------
-
+	var c = gl.getAttribLocation(prog, 'clr');
+	gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+	gl.vertexAttribPointer(c, 4, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(c);
 
     /******* 6.0 REENDERIZAMOS  *******/
 	// Ahora que ya está todo seteado, renderizamos la escena. 
 	// El primer paso es siempre limpiar la imagen. 
 	// Cada vez que cambie la escena, tenemos que reenderizar nuevamente. 
-	// TODO <-----------
-
-
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	gl.useProgram(prog);
+	gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
